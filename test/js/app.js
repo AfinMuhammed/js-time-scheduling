c54bdbef51a6b4eb3451352257b9824240113245
AfinMuhammed <afin.ta@techversantinfo.com>
2022-05-10 19:25:07 +0530

    updated

--- test/js/app.js
+++ test/js/app.js
@@ -36,36 +36,24 @@ var data = [{
   title: "Push up branch"
 }];
 
-console.log(data[1].start + 60 * 8);
-var a = data[0].start + 60 * 8;
-var b = Math.floor(a / 60);
-var c = a % 60;
-console.log(b + ":" + c);
-// console.log(data[0].start);
 
+console.log(data[0].title);
+var a = 60 * 8;
 
 for (var i = 0; i < 10; i++) {
-  $(".main").after(`<div class="TimeSection">
-  <div class="firsthalf d-flex">
-    <span class="time">8:00</span>
+  // console.log(data[i].start + 60 * 8);
+
+var hour = Math.floor((a+i*30) / 60);
+var min = (a+i*30) % 60;
+// var duration= ( i==0 ) ? 0 : data[i--].duration;
+// console.log(b + ":" + c);
+  $(".main").append(`<div class="TimeSection">
+  <div class=" d-flex">
+    <span class="time">${hour + ":" + min}</span>
     <div class="timeContent">
-      <div class=" contentActive firstOne">
-        Exercise
+      <div class=" contentActive" style="height:${data[i].duration}px;margin-top:${data[i].start}px;">
+        ${data[i].title}
       </div>
-      <div class=" d-block h-33"></div>
-      <div class="d-flex  h-16">
-        <div class=" w-50"></div>
-        <div class="contentActive w-50 ms-0"></div>
-      </div>
-    </div>
-  </div>
-  <div class="secondhalf d-flex">
-    <span class="time">8:30</span>
-    <div class="timeContent contentActive planDay h-100 w-50">
-      Plan day
-    </div>
-    <div class="timeContent contentActive ms-0 travelWork w-50">
-      Travel to work
     </div>
   </div>
 </div>`)

4294b058dfb92bd996de6fd6e01977a99ca1806f
AfinMuhammed <afin.ta@techversantinfo.com>
2022-05-13 18:30:40 +0530

    updated

--- test/js/new.js
+++ test/js/new.js
@@ -34,10 +34,6 @@ var data = [{
   start: 400,
   duration: 30,
   title: "Push up branch"
-}, {
-  start: 430,
-  duration: 10,
-  title: "commented"
 }];
 
 $(".main").append(`<div class="TimeSection">
@@ -56,12 +52,11 @@ for (var i = 0; i < 10; i++) {
   var currentTimeTopHour = (i * 120) + 60;
   var currentTimeTopMin = (i * 120) + 120;
   var hour = Math.floor((firststart + i * 60) / 60);
-  if(hour>12)
-    var hour = ((hour + 11) % 12 + 1);
+  if (hour > 12) var hour = ((hour + 11) % 12 + 1);
   var min = (firststart + i * 60) % 60;
   var currentHrTagTop = (i * 120) + 45;
   var hour1 = Math.floor((secondstart + i * 60) / 60);
-  if(hour1>12)
+  if (hour1 > 12)
     var hour1 = ((hour1 + 11) % 12 + 1);
   var min1 = (secondstart + i * 60) % 60;
   $('.TimeSection').append(`<hr style="top:${currentHrTagTop}px">`);
@@ -70,24 +65,20 @@ for (var i = 0; i < 10; i++) {
 }
 
 for (i = 0; i < data.length; i++) {
-  // for (j = 0; j < data.length - 1; j++) {
-  //   if (j == i) {
-  //     continue;
-  //   } 
-  //  else {}
-  // }
-      // var nexteventStart = data[j].start;
-      var currentEventDuration = (data[i].duration) * 2 + 'px';
-      var nexteventStart = data[i+1].start;
-      var currentEventTop = ((data[i].start) * 2) + 60;
-      var isOverlapping = nexteventStart - (data[i].start + data[i].duration);
-      if (isOverlapping < 0) {
-        data[i+1].overlapped = true;
-        var addTask = `<div class=" contentActive " style="height:${currentEventDuration};width:450px;top:${currentEventTop}px;left:660px">${data[i].title}</div>`;
-      } else
-        var addTask = `<div class=" contentActive " style="height:${currentEventDuration};width:900px;top:${currentEventTop}px;left:210px">${data[i].title}</div>`;
-      if (data[i].overlapped)
-        var addTask = `<div class=" contentActive " style="height:${currentEventDuration};width:450px;top:${currentEventTop}px;left:210px">${data[i].title}</div>`;
-      $('.firsthalf').find('.timeContent').append(addTask);
-    
+  var currentEventDuration = (data[i].duration) * 2 + 'px';
+  var nexteventStart = (data[i + 1]) ? data[i + 1].start : (data[i].start + data[i].duration);
+  var currentEventTop = ((data[i].start) * 2) + 60;
+  var isOverlapping = nexteventStart - (data[i].start + data[i].duration);
+  if (isOverlapping <= 0) {
+    if(data[i + 1]) data[i + 1].overlapped = true;
+    var addTask = `<div class=" contentActive " style="height:${currentEventDuration};width:450px;top:${currentEventTop}px;left:660px">${data[i].title}</div>`;
+  } else{
+    var addTask = `<div class=" contentActive " style="height:${currentEventDuration};width:900px;top:${currentEventTop}px;left:210px">${data[i].title}</div>`;
+  }
+  //if (data[i].overlapped && !data[i].isPloated)  
+  if (data[i].overlapped && data[i+1])
+    var addTask = `<div class=" contentActive " style="height:${currentEventDuration};width:450px;top:${currentEventTop}px;left:210px">${data[i].title}</div>`;
+  $('.firsthalf').find('.timeContent').append(addTask);
+  data[i].isPloated=true;
+
 }
